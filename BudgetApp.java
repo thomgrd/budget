537200ae89ce64e4afabd2b2d7547a3620f3778d
thomgrd <128264872+thomgrd@users.noreply.github.com>
2023-03-28 23:14:34 -0400

    Add files via upload

--- BudgetApp.java
+++ BudgetApp.java
@@ -10,73 +10,8 @@ import java.util.List;
 public class BudgetApp extends JFrame {
 
     private JPanel contentPane;
-    private List<Budget> budgets;
-    private List<Expense> expenses;
-
-    public BudgetApp() {
-        setTitle("Budget App");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 800, 600);
-        contentPane = new JPanel();
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        setContentPane(contentPane);
-        contentPane.setLayout(new BorderLayout());
-
-        JLabel balanceLabel = new JLabel("Total Balance: $0.00");
-        balanceLabel.setFont(new Font("Arial", Font.PLAIN, 20));
-        balanceLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        contentPane.add(balanceLabel, BorderLayout.NORTH);
-
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new FlowLayout());
-
-        JButton budgetBtn = new JButton("Create Budget");
-        budgetBtn.addActionListener(e -> addBudget());
-        buttonPanel.add(budgetBtn);
-
-        JButton expenseBtn = new JButton("Add Expense");
-        expenseBtn.addActionListener(e -> addExpense());
-        buttonPanel.add(expenseBtn);
-
-        contentPane.add(buttonPanel, BorderLayout.SOUTH);
-
-        JPanel listPanel = new JPanel();
-        listPanel.setLayout(new BoxLayout(listPanel, BoxLayout.Y_AXIS));
-
-        JScrollPane budgetScrollPane = new JScrollPane();
-        budgetScrollPane.setBorder(BorderFactory.createTitledBorder("Budgets"));
-        listPanel.add(budgetScrollPane);
-
-        JList<Budget> budgetList = new JList<>();
-        budgetScrollPane.setViewportView(budgetList);
-
-        JScrollPane expenseScrollPane = new JScrollPane();
-        expenseScrollPane.setBorder(BorderFactory.createTitledBorder("Expenses"));
-        listPanel.add(expenseScrollPane);
-
-        JList<Expense> expenseList = new JList<>();
-        expenseScrollPane.setViewportView(expenseList);
-
-        contentPane.add(listPanel, BorderLayout.CENTER);
-
-        // Initialize lists
-        budgets = new ArrayList<Budget>();
-        expenses = new ArrayList<Expense>();
-    }
-
-    private void addBudget() {
-        Budget budget = new Budget();
-        budgets.add(budget);
-        BudgetDialog dialog = new BudgetDialog(this, budget);
-        dialog.setVisible(true);
-    }
-
-    private void addExpense() {
-        Expense expense = new Expense();
-        expenses.add(expense);
-        ExpenseDialog dialog = new ExpenseDialog(this, expense);
-        dialog.setVisible(true);
-    }
+    private List<Budget> budgets = new ArrayList<>();
+    private List<Expense> expenses = new ArrayList<>();
 
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
@@ -86,8 +21,78 @@ public class BudgetApp extends JFrame {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            
         });
+    }
+
+    public BudgetApp() {
+        setTitle("Budget App");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 600, 400);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(new GridBagLayout());
+
+        JLabel titleLabel = new JLabel("Welcome to Budget App");
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        GridBagConstraints gbc_titleLabel = new GridBagConstraints();
+        gbc_titleLabel.fill = GridBagConstraints.HORIZONTAL;
+        gbc_titleLabel.insets = new Insets(0, 0, 20, 0);
+        gbc_titleLabel.gridwidth = 2;
+        gbc_titleLabel.gridx = 0;
+        gbc_titleLabel.gridy = 0;
+        contentPane.add(titleLabel, gbc_titleLabel);
+
+        JButton startButton = new JButton("Get Started");
+        startButton.setFont(new Font("Arial", Font.PLAIN, 16));
+        GridBagConstraints gbc_startButton = new GridBagConstraints();
+        gbc_startButton.insets = new Insets(0, 0, 0, 5);
+        gbc_startButton.gridx = 0;
+        gbc_startButton.gridy = 1;
+        contentPane.add(startButton, gbc_startButton);
+
+        JButton aboutButton = new JButton("About");
+        aboutButton.setFont(new Font("Arial", Font.PLAIN, 16));
+        GridBagConstraints gbc_aboutButton = new GridBagConstraints();
+        gbc_aboutButton.gridx = 1;
+        gbc_aboutButton.gridy = 1;
+        contentPane.add(aboutButton, gbc_aboutButton);
+
+        JLabel footerLabel = new JLabel("Budget App © 2023 All rights reserved.");
+        footerLabel.setFont(new Font("Arial", Font.PLAIN, 12));
+        footerLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        GridBagConstraints gbc_footerLabel = new GridBagConstraints();
+        gbc_footerLabel.fill = GridBagConstraints.HORIZONTAL;
+        gbc_footerLabel.insets = new Insets(20, 0, 0, 0);
+        gbc_footerLabel.gridwidth = 2;
+        gbc_footerLabel.gridx = 0;
+        gbc_footerLabel.gridy = 2;
+        contentPane.add(footerLabel, gbc_footerLabel);
+
+        startButton.addActionListener(e -> {
+            HomePage homePage = new HomePage();
+            homePage.setVisible(true);
+        });
+
+        aboutButton.addActionListener(e -> JOptionPane.showMessageDialog(this, "This application was created by Budget App Team"));
+
+        addBudget();
+        addExpense();
+    }
+
+    private void addBudget() {
+        String input = JOptionPane.showInputDialog(this, "Enter budget amount:");
+        if (input != null && !input.isEmpty()) {
+            try {
+                double budgetAmount = Double.parseDouble(input);
+                Budget budget = new Budget(budgetAmount);
+                budgets.add(budget);
+                updateBudgetsPanel();
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(this, "Invalid input. Please enter a number.");
+            }
+        }
     }
 }
 
